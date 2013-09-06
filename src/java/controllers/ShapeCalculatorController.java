5025f891a6f387552e7316ee6e03d30495214935
Joe Rankin <jrankin2@my.wctc.edu>
2013-09-05 19:26:24 -0500

    finished validation, ajax request, css/jqueryui

--- src/java/controllers/ShapeCalculatorController.java
+++ src/java/controllers/ShapeCalculatorController.java
@@ -19,7 +19,6 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "ShapeCalculator", urlPatterns = {"/ShapeCalculator"})
 public class ShapeCalculatorController extends HttpServlet {
-
     private static final String RESULT_PAGE = "result.jsp";
     private static final double NULL_VALUE = -1;
 
@@ -38,83 +37,15 @@ public class ShapeCalculatorController extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
-            double result = getResult(request);
+            double result = ShapeCalculatorService.calculateFromRequest(request);
             request.setAttribute("result", result);
             RequestDispatcher view = request.getRequestDispatcher(RESULT_PAGE);
             view.forward(request, response);
-
         } finally {
         }
     }
 
-    private double getParameterDouble(HttpServletRequest request, String name) {
-        String nameValue = request.getParameter(name).trim();
-        return (nameValue != null && !nameValue.isEmpty()
-                ? Double.parseDouble(request.getParameter(name))
-                : NULL_VALUE);
-    }
-
-    private double getResult(HttpServletRequest request) {
-        double result = -1;
-        String sParam = request.getParameter("s");
-        if (sParam != null) {
-            Shapes shape = Shapes.toShape(Integer.parseInt(sParam));
-            if (shape == null) {
-                //invalid s value
-            }
-            switch (shape) {
-                case RECTANGLE:
-                    double width = getParameterDouble(request, "width");
-                    double length = getParameterDouble(request, "length");
-                    //double width = Double.parseDouble(request.getParameter("width"));
-                    //double length = Double.parseDouble(request.getParameter("length"));
-                    if (width != NULL_VALUE && length != NULL_VALUE) {
-                        result = width * length;
-                    } else {
-                        //error?
-                    }
-
-                    break;
-                case CIRCLE:
-                    double radius = getParameterDouble(request, "radius");
-                    //double radius = Double.parseDouble(request.getParameter("radius"));
-                    if (radius != NULL_VALUE) {
-                        result = Math.PI * Math.pow(radius, 2);
-                    } else {
-                        //error
-                    }
-                    break;
-                case TRIANGLE:
-                    double a = getParameterDouble(request, "a");
-                    double b = getParameterDouble(request, "b");
-                    double c = getParameterDouble(request, "c");
-                    /*
-                     String aStr = request.getParameter("a");
-                     String bStr = request.getParameter("b");
-                     String cStr = request.getParameter("c");
-                     double a = Double.parseDouble(aStr);
-                     double b = Double.parseDouble(bStr);
-                     double c = Double.parseDouble(cStr);
-                     */
-                    if (c == NULL_VALUE) {
-                        result = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-                    } else {
-                        if (a != NULL_VALUE) {
-                            result = Math.sqrt(Math.pow(a, 2) - Math.pow(c, 2));
-                        } else if (b != NULL_VALUE) {
-                            result = Math.sqrt(Math.pow(b, 2) - Math.pow(c, 2));
-                        } else {
-                            //error
-                        }
-                    }
-                    break;
-            }
-
-            
-        }
-        return result;
-    }
-
+    
 
 
         // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
--- web/result.jsp
+++ web/result.jsp
@@ -6,15 +6,4 @@
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <%@page import="java.util.*" %>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Calculator Results</title>
-    </head>
-    <body>
-        <h1>Calculator Results</h1>
-        Result: <% out.print(request.getAttribute("result")); %>
-        <br/>
-    </body>
-</html>
+<p style="text-align:center;"><% out.print(request.getAttribute("result")); %></p>
